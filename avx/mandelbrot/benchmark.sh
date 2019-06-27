02b848140a353516f01c0e04211a8930b691bbb3
Joel Luth <joel.luth@emc.com>
2019-06-27 11:38:21 -0500

    Daniel Lemire's AVX512 mandelbrot test
    From https://lemire.me/blog/2018/08/24/trying-harder-to-make-avx-512-look-bad-my-quantified-and-reproducible-results/

---
+++ avx/mandelbrot/benchmark.sh
@@ -0,0 +1,7 @@
+for i in {1..20}; do time ./mandelbrotzero ${i} 3200; done
+sleep 2
+for i in {1..20}; do time ./mandelbrot ${i} 3200; done
+sleep 2
+for i in {1..20}; do time ./mandelbrot512 ${i} 3200; done
+sleep 2
+for i in {1..20}; do time ./mandelbrotheavy512 ${i} 3200; done
