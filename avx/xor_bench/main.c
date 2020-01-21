db18c9959d2df27f7efd6da9bd15744007b39830
Joel Luth <joel.luth@dell.comm>
2020-01-20 21:44:48 -0600

    add a loop to get some runtime

--- avx/xor_bench/main.c
+++ avx/xor_bench/main.c
@@ -1,12 +1,20 @@
 #include "xor.h"
 
 #define SIZE (64 * 1024)
+#define LOOPS 1024 * 1024
 
 int main(int argc, char *argv[])
 {
 	char r1[SIZE+1] = "",
     r2[SIZE+1] = "",
     r3[SIZE+1] = "";
-    xor_byte(r1 ,r2, r3, SIZE);
-    xor_sse2(r1 ,r2, r3, SIZE);
+    int j;
+    for (j=0; j<LOOPS; j++)
+    {
+        xor_byte(r1 ,r2, r3, SIZE);
+    }
+    for (j=0; j<LOOPS; j++)
+    {
+        xor_sse2(r1 ,r2, r3, SIZE);
+    }
 }
